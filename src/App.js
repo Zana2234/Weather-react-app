4dd3f5befee0ef8d0895f870cfe52a7c9b6b80cf
Ana_Z <113620777+Zana2234@users.noreply.github.com>
2024-04-16 14:41:36 +0200

    Added structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Wetaher App
+      <footer>
+        This Project was coded by Ana Zabaleta and is
+        <a href="">open-sourced on GitHub</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
